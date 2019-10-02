46d35f23156fe36252d44306bf7df3f2e96101f8
Torben <torben.letorbi@gmail.com>
2019-10-02 23:15:48 +0200

    Extract synoindex_recursive from https://github.com/letorbi/synoindexwatcher

---
+++ README.md
@@ -0,0 +1,7 @@
+# Inotify_recursive - Recursive inotify watches for Python
+
+TODO
+
+----
+
+Copyright 2019 Torben Haase \<[https://pixelsvsbytes.com](https://pixelsvsbytes.com)>
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if sys.version_info.major < 3:
+    install_requires = ["enum", "inotify_simple"]
+else:
+    install_requires = ["inotify_simple"]
+
+setuptools.setup(
+    name="inotify_recursive",
+    version="0.1.0",
+    author="Torben Haase",
+    author_email="torben@pixelsvsbytes.com",
+    description="Recursive inotify watches based on inotify_simple.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/letorbi/inotify_recursive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=install_requires,
+)
