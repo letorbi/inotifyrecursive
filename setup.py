892cf98f7dfba14eeedd95eefa343c7577187691
Torben <git@letorbi.com>
2020-02-21 22:20:09 +0100

    Update install_requires and author_email; Add python_requires

--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_requires = ["inotify-simple"]
-
-# The enum34 requirement should be handled by inotify-simple
-if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 4):
-    install_requires += ["enum34"]
-
 setuptools.setup(
     name="inotifyrecursive",
     version="0.2.6",
     author="Torben Haase",
-    author_email="torben@pixelsvsbytes.com",
+    author_email="git@letorbi.com",
     description="Recursive inotify watches based on inotify_simple.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -28,5 +22,6 @@ setuptools.setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
     ],
-    install_requires=install_requires,
+    install_requires=["inotify-simple"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*"
 )
