d6309bc8baaeb4588b6c13f5eab22067d145933f
Torben <git@letorbi.com>
2020-11-20 13:12:14 +0100

    Bump version to 0.3.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="inotifyrecursive",
-    version="0.3.4",
+    version="0.3.5",
     author="Torben Haase",
     author_email="git@letorbi.com",
     description="Recursive inotify watches based on inotify_simple.",
